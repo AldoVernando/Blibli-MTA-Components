cf7df32c7dc466d2de0524b0cb410a66dedff6b9
Aldo Vernando <aldo.vernando@gmail.com>
2020-05-06 06:28:24 +0700

    Initial Commit

---
+++ Blibli MTA Components/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blibli MTA Components
+//
+//  Created by TEMP on 5/6/20.
+//  Copyright © 2020 aldovernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
